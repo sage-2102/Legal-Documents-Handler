d7395c03280c6482785bcb5541e7027eaaaf4892
Zoya Pathan <zoyapathan2630@gmail.com>
2026-06-21 16:47:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://legalconnect-api-rkhu.onrender.com"
 
 st.set_page_config(
     page_title="LegalConnect",
